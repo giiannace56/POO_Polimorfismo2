38d2c4eadfb388e9c0193b9d8ec1dcceb714f687
DEV <dev@info.local>
2020-12-11 15:03:44 -0300

    sobrecarga finalizada

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using aula_11_12_presencial.classes;
+
+namespace aula_11_12_presencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario fun = new Funcionario();
+            fun.Mostrar();
+            fun.Mostrar("paulo");
+            fun.Mostrar(2);
+
+        }
+    }
+}
